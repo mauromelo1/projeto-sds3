f37e03668d1fd3428b0288ab6c7ac40d04006b06
Mauro Melo <mauromelo1@gmail.com>
2021-05-03 15:33:12 -0300

    Project created

---
+++ backend/src/main/java/com/mauromelo1/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.mauromelo1.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
